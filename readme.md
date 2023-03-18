d2133263de51e52e10ee3fed563a0f38900be121
Rodrigo Alves Vilela <23285553+imrodrigoalves@users.noreply.github.com>
2023-03-18 12:08:57 +0000

    .

---
+++ readme.md
@@ -0,0 +1,13 @@
+Following youtube tutorial:
+
+https://www.youtube.com/watch?v=U34l-Xz5ynU
+
+You will learn:
+- How to implement a very simple AI to make the creatures feel alive
+- How to control the FPS of our game and how to measure time to trigger periodic events
+- How to restart the game by pressing a button
+- How to apply collision detection, resolve collisions and use that to simulate physics
+- How to use the built-in drawImage method to draw randomized game environments and animated characters from a sprite sheet
+- How to capture mouse position and animate an 8 directional sprite sheet based on the relative position between the mouse and the player character
+- How to use HTML5, CSS3 and plain vanilla JavaScript to build a game from scratch. We will write and understand every line of code, we will not rely on any external frameworks or libraries
+... and much more
---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1280;
+  canvas.height = 720;
+
+  ctx.fillStyle = "white";
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 30;
+    }
+
+    draw(context) {
+      context.beginPath();
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      );
+      context.fill();
+    }
+  }
+
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.player = new Player(this);
+    }
+
+    render(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas);
+  game.render(ctx);
+
+  // Animation loop to draw and update game
+  function animate() {}
+});
