37fecc0ff37aa52209db5985a6738347db633be8
Rodrigo Alves Vilela <23285553+imrodrigoalves@users.noreply.github.com>
2023-03-21 20:04:41 +0000

    Drawing settings

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ window.addEventListener("load", () => {
   canvas.height = 720;
 
   ctx.fillStyle = "white";
-
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'white';
   class Player {
     constructor(game) {
       this.game = game;
-      this.collisionX = this.game.width * 0.5;
-      this.collisionY = this.game.height * 0.5;
-      this.collisionRadius = 30;
+      this.collisionX = this.game.width * 0.5; // Defines position on X axis
+      this.collisionY = this.game.height * 0.5; // Defines position on Y axis
+      this.collisionRadius = 30; // Defines size of object
     }
 
     draw(context) {
@@ -23,7 +24,16 @@ window.addEventListener("load", () => {
         0,
         Math.PI * 2
       );
+      
+      // save() and restore() applies specific settings to selected shapes
+      // without affecting other drawings
+      
+      context.save(); // Creates a snapshot of the current canvas status
+      context.globalAlpha = 0.5; // Sets opacity of the shapes
       context.fill();
+      context.restore(); // Restores previous status
+      
+      context.stroke();
     }
   }
 
